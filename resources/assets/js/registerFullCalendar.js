110f2a33ceb0569ad023447aae0544df9a60ae73
Ragkhuza <dargonhelsing@gmail.com>
2017-12-08 11:36:09 +0800

    fixing bug when deleting events (part2)

--- resources/assets/js/registerFullCalendar.js
+++ resources/assets/js/registerFullCalendar.js
@@ -114,11 +114,11 @@ $(function () {
             //if borderTop is red remove it
             if ($hex === '#ff0000') {
                 axios.post('/resource/events/', {
-                    _method: 'delete',
                     title: calEvent.title,
                     start: calEvent.start._d.getTime() / 1000,
                     end: calEvent.start._d.getTime() / 1000,
                     backgroundColor: rgbToHex(calEvent.backgroundColor),
+                    '_method': 'delete',
                 }).then(function (response) {
                     $('#calendar').fullCalendar('removeEvents', calEvent._id);
                     console.log(response);
