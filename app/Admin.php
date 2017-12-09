08ec041666effb679f734e2f780a3209d3de1933
Ragkhuza <dargonhelsing@gmail.com>
2017-12-09 17:54:53 +0800

    defined Mutator and accessor for teacher and student model

--- app/Admin.php
+++ app/Admin.php
@@ -5,9 +5,18 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 class Admin extends Authenticatable
 {
     use Notifiable;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
 
     protected $fillable = [
         'username', 'password',
--- app/Teacher.php
+++ app/Teacher.php
@@ -13,10 +13,18 @@ use App\Traits\Rateable;
  * @property \Carbon\Carbon $updated_at
  * @property string $full_name
  * @property mixed $assignments
+ * @property mixed $first_name
+ * @property mixed $middle_name
+ * @property mixed $last_name
  */
 class Teacher extends Authenticatable
 {
     use Notifiable, Rateable;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
 
     protected $fillable = [
         'username', 'password', 'first_name', 'middle_name','last_name', 'age', 'advisory',
@@ -25,6 +33,45 @@ class Teacher extends Authenticatable
     protected $hidden = [
         'remember_token', 'created_at', 'updated_at',
     ];
+
+    /*GETTERS*/
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->middle_name . ' ' . $this->last_name;
+    }
+
+    public function getFirstNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    public function getMiddleNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    public function getLastNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+    /*END OF GETTERS*/
+
+    /*SETTERS*/
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['first_name'] = strtolower($value);
+    }
+
+    public function setMiddleNameAttribute($value)
+    {
+        $this->attributes['middle_name'] = strtolower($value);
+    }
+
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['last_name'] = strtolower($value);
+    }
+    /*END OF SETTERS*/
 
     public function subjects()
     {
@@ -45,11 +92,6 @@ class Teacher extends Authenticatable
             return 0;
         }
         return $rating->rating;
-    }
-
-    public function getFullNameAttribute()
-    {
-        return $this->first_name . ' ' . $this->middle_name . ' ' . $this->last_name;
     }
 
     public function assignments()
